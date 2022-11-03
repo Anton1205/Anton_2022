eca380b90799a6edd05bf91042bbd755b35712b1
Anton <antonchik18@icloud.com>
2022-11-03 21:46:52 +0300

    next_files

--- Anton_2022/blog/models.py
+++ Anton_2022/blog/models.py
@@ -14,5 +14,4 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
